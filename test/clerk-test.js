0c935cacaa6d98757a99528791b204b6b5a32742
Michael Phan-Ba <michael@mikepb.com>
2012-04-28 17:37:17 -0700

    clerk and package.json versions should match

--- test/clerk-test.js
+++ test/clerk-test.js
@@ -1,12 +1,33 @@
 if (typeof require != 'undefined') {
   var clerk = require('..')
-    , expect = require('expect.js');
+    , expect = require('expect.js')
+    , fs = require('fs');
 }
 
 describe('clerk', function(){
 
   beforeEach(function(){
     this.client = new clerk.Client('http://127.0.0.1:5984');
+  });
+
+  if (fs) describe('package', function(){
+
+    before(function(done){
+      var self = this;
+      fs.readFile('package.json', function(err, source){
+        self.source = source.toString();
+        done(err);
+      });
+    });
+
+    before(function(){
+      this.package = JSON.parse(this.source);
+    });
+
+    it('should match package.json version', function(){
+      expect(clerk).to.have.property('version', this.package.version);
+    });
+
   });
 
 });
